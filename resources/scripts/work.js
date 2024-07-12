d64f29b39986d0122a20b0021342b20055bdd6a6
SONG IL YOUNG <siyno1@ampel.co.kr>
2024-07-12 10:55:20 +0900

    work 작업중

---
+++ resources/scripts/work.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    // 포트폴리오 버튼
+    var btnDetails = document.querySelectorAll('.detail');
+
+    btnDetails.forEach(el => el.addEventListener('mousemove', function(e) {
+        const pos = this.getBoundingClientRect();
+        const mx = e.clientX - pos.left - pos.width/2;
+        const my = e.clientY - pos.top - pos.height/2;
+
+        this.style.transform = 'translate('+ mx * 0.15 +'px, '+ my * 0.3 +'px)';
+        this.style.transform += 'rotate3d('+ mx * -0.1 +', '+ my * -0.3 +', 0, 12deg)';
+        //this.children[0].style.transform = 'translate('+ mx * 0.025 +'px, '+ my * 0.075 +'px)';
+    }));
+
+    btnDetails.forEach(el => el.addEventListener('mouseleave', function() {
+        this.style.transform = 'translate3d(0px, 0px, 0px)';
+        this.style.transform += 'rotate3d(0, 0, 0, 0deg)';
+        //this.children[0].style.transform = 'translate3d(0px, 0px, 0px)';
+    }));
+});
+
