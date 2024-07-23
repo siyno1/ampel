db00bdd13d47ce0feff3749fb1682ece5a6b9850
SONG IL YOUNG <siyno1@ampel.co.kr>
2024-07-23 16:23:01 +0900

    스크롤 개선

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -11,7 +11,8 @@ var Ampel = {
     **/
     lenis:new Lenis({
         duration: 1.5,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        syncTouch:true
     }),
     raf:function(time){
         Ampel.lenis.raf(time);
