00d73b88d96c875cbc8d79980796b8cd23b12333
SONG IL YOUNG <siyno1@ampel.co.kr>
2024-07-29 13:43:14 +0900

    메인 스크립트 정리

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -4,17 +4,17 @@
  */
 
 var Ampel = {
-    isMobile:navigator.userAgent.match(/(iPhone|iPod|iPad|Android)/) !== null,
+    isMobile: navigator.userAgent.match(/(iPhone|iPod|iPad|Android)/) !== null,
     /*
      * lenis
      * scroll 및 인터렉션 관련 필수요소
     **/
-    lenis:null,
-    raf:function(time){
+    lenis: null,
+    raf: function (time) {
         Ampel.lenis.raf(time);
         requestAnimationFrame(Ampel.raf);
     },
-    init:function(){
+    init: function () {
         Ampel.lenis = new Lenis({
             duration: 1.5,
             easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
@@ -22,9 +22,9 @@ var Ampel = {
 
         requestAnimationFrame(Ampel.raf);
 
-        gsap.ticker.add((time)=>{
+        gsap.ticker.add((time) => {
             Ampel.lenis.raf(time * 1000);
-        })
+        });
 
         gsap.ticker.lagSmoothing(0);
         gsap.registerPlugin(ScrollTrigger);
@@ -32,38 +32,45 @@ var Ampel = {
         ScrollTrigger.normalizeScroll(true);
         ScrollTrigger.config({ ignoreMobileResize: true });
 
-        window.addEventListener("resize", ScrollTrigger.update);
+        window.addEventListener("resize", this.debounce(() => {
+            ScrollTrigger.update();
+            Ampel.resetAnimations();
+        }, 200));
+
+        this.logoAnimation();
+        this.marquee();
+        this.setGnbAnimation();
     },
     /**
      * 헤더 로고 애니메이션
      */
-    logoAnimation:function(){
-        var logo4Img = document.querySelector(".lg4 img")
+    logoAnimation: function () {
+        var logo4Img = document.querySelector(".lg4 img");
         var logos = document.querySelector("h1.header_logo");
         var logosImg = logos.querySelectorAll("img");
 
-        logosImg.forEach(function(element, index){
+        logosImg.forEach(function (element, index) {
             if (index === 3) return;
             gsap.to(element, {
-                delay:0.5,
-                css:{
-                    transform:"translate(0,0)"
+                delay: 0.5,
+                css: {
+                    transform: "translate(0,0)"
                 }
             });
         });
 
         gsap.fromTo(logo4Img, {
-            css:{
-                transform:"translate(130%,0)"
+            css: {
+                transform: "translate(130%,0)"
             },
-        },{
-            css:{
-                transform:"translate(0,0)"
+        }, {
+            css: {
+                transform: "translate(0,0)"
             },
             scrollTrigger: {
-                trigger:logos,
-                start:"top top",
-                end:"+=100",
+                trigger: logos,
+                start: "top top",
+                end: "+=100",
                 markers: false,
                 scrub: 1
             }
@@ -72,26 +79,19 @@ var Ampel = {
     /**
      * 상단 바로가기
      */
-    goTop:function(){
+    goTop: function () {
         gsap.to(window, { duration: 1, scrollTo: 0 });
     },
-    marquee:function(){
+    marquee: function () {
         var marquee = document.querySelectorAll('.marquee');
 
-        if(!marquee) return
+        if (!marquee) return;
         marquee.forEach(item => {
             var marqueeContent = item.querySelector('span');
 
-            // Duration
             var duration = 10;
 
-            // Element Clone
-            // var marqueeContentClone = marqueeContent.cloneNode(true);
-            // item.append(marqueeContentClone);
-
-            // Marquee animation
             var marqueeContentAll = item.querySelectorAll('span');
-
 
             if (item.classList.contains('reverse')) {
                 marqueeContentAll.forEach(element => {
@@ -100,8 +100,8 @@ var Ampel = {
                         repeat: -1,
                         duration: duration,
                         ease: 'linear'
-                    })
-                })
+                    });
+                });
             } else {
                 marqueeContentAll.forEach(element => {
                     gsap.to(element, {
@@ -109,73 +109,86 @@ var Ampel = {
                         repeat: -1,
                         duration: duration,
                         ease: 'linear'
-                    })
-                })
+                    });
+                });
             }
 
-        })
+        });
+    },
+    resetAnimations: function () {
+        // 모든 트윈을 중지
+        ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+
+        gsap.killTweensOf("*");
+        // gsap.killTweensOf(this.marquee());
+        this.logoAnimation(); // 로고 애니메이션 재시작
+        this.marquee(); // 마퀴 애니메이션 재시작
+        this.setGnbAnimation();
+    },
+    debounce: function (func, wait) {
+        let timeout;
+        return function (...args) {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => func.apply(this, args), wait);
+        };
+    },
+    gnbAnimation:null,
+    setGnbAnimation:function(){
+        var $gnb = document.querySelector(".gnb_mobile");
+        var $menus = $gnb.querySelectorAll("a");
+        var $copyright = $gnb.querySelector(".copyright");
+
+        if (this.gnbAnimation) this.gnbAnimation.killTweensOf("*");
+
+        this.gnbAnimation = gsap.timeline({ paused: true });
+        this.gnbAnimation.fromTo($gnb, {
+            'will-change': 'opacity, transform',
+            transformOrigin: "0% 100%",
+            opacity: 0,
+            scaleY: 0,
+            display: "none"
+        }, {
+            duration: 0.5,
+            scaleY: 1,
+            opacity: 1,
+            display: "block"
+        }).fromTo($menus, {
+            'will-change': 'opacity, transform',
+            y: 20,
+            opacity: 0
+        }, {
+            y: 0,
+            opacity: 1,
+            stagger: 0.1
+        }, 0.4).fromTo($copyright, {
+            'will-change': 'opacity',
+            opacity: 0
+        }, {
+            opacity: 1
+        }, 0.2);
+    },
+    gnbOpen:function(){
+        if(Ampel.gnbAnimation) Ampel.gnbAnimation.play();
+    },
+    gnbClose:function(){
+        if(Ampel.gnbAnimation) Ampel.gnbAnimation.reverse();
     }
 }
 
 /**
  * 사이트 전역에 실행되는 스크립트
  */
-$(document).ready(function(){
+$(document).ready(function () {
     /**
      * 이미지까지 로드 이후에 스크립트 실행
      */
-    $(window).on("load", function(){
+    $(window).on("load", function () {
         Ampel.init();
-        Ampel.logoAnimation();
-        Ampel.marquee();
 
         // ScrollTrigger.normalizeScroll(true);
         //상단 바로가기 버튼
-        var $btnTop = $(".btn_top");
-        $btnTop.on("click", Ampel.goTop);
-
-        var $gnb = document.querySelector(".gnb_mobile");
-        var $menus = $gnb.querySelectorAll("a");
-        var $copyright = $gnb.querySelector(".copyright");
-
-        var gnbTl = gsap.timeline({
-            paused:true
-        });
-
-        gnbTl.fromTo($gnb, {
-            'will-change': 'opacity, transform',
-            transformOrigin:"0% 100%",
-            opacity:0,
-            scaleY:0,
-            display:"none"
-        },{
-            duration:0.5,
-            scaleY:1,
-            opacity:1,
-            display:"block"
-        }).fromTo($menus, {
-            'will-change': 'opacity, transform',
-            y:20,
-            opacity:0
-        },{
-            y:0,
-            opacity:1,
-            stagger:0.1
-        },0.4).fromTo($copyright, {
-            'will-change': 'opacity',
-            opacity:0
-        },{
-            opacity:1
-        },0.2);
-
-
-        $(".btn_gnb").on("click", function(){
-            gnbTl.play();
-        });
-
-        $(".btn_gnb_close").on("click", function(){
-            gnbTl.reverse();
-        });
-
+        $(".btn_top").on("click", Ampel.goTop);
+        $(".btn_gnb").on("click",Ampel.gnbOpen);
+        $(".btn_gnb_close").on("click", Ampel.gnbClose);
     });
 });//@ready
