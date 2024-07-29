7bc7f743b370c1fb7a213eeeb6888b202e89ce73
SONG IL YOUNG <siyno1@ampel.co.kr>
2024-07-29 17:27:19 +0900

    수정

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -12,12 +12,14 @@ var Ampel = {
     lenis: null,
     raf: function (time) {
         Ampel.lenis.raf(time);
+        ScrollTrigger.update();
         requestAnimationFrame(Ampel.raf);
     },
     init: function () {
         Ampel.lenis = new Lenis({
-            duration: 1.5,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+            duration: 1,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            smooth: true
         });
 
         requestAnimationFrame(Ampel.raf);
